2270f58a306ec70f95d6f69351ed76dbb81a7768
Adrian Waddell <adrian.waddell@roche.com>
2021-10-20 14:38:39 +0200

    restructure content

--- ADaM.R
+++ ADaM.R
@@ -9,10 +9,13 @@ SDTM <- list(
   MH = rtables::ex_admh %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, MHSEQ, MHTERM, MHDECOD, MHBODSYS)
 )
 
+saveRDS(rtables::ex_adsl %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM), "data_demo/DM.rds")
+saveRDS(rtables::ex_adae %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, AESEQ, AETERM, AEDECOD), "data_demo/AE.rds")
+saveRDS(rtables::ex_admh %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, MHSEQ, MHTERM, MHDECOD, MHBODSYS), "data_demo/MH.rds")
 
-
-
-
+saveRDS(rtables::ex_adsl, "data_demo/ADSL.rds")
+saveRDS(rtables::ex_adae, "data_demo/ADAE.rds")
+saveRDS(rtables::ex_admh, "data_demo/ADMH.rds")
 ADaM <- list(
   ADSL = rtables::ex_adsl,
   ADAE = rtables::ex_adae,
