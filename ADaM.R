86422d5a2e0dd54402817e7f6e415a80809f8eab
Adrian Waddell <adrian.waddell@roche.com>
2021-10-20 14:02:37 +0200

    some more preparation

--- ADaM.R
+++ ADaM.R
@@ -1,4 +1,20 @@
 
 library(admiral)           # https://roche-gsk.github.io/admiral/
+library(dplyr)
+
+# source("SDTM.R") # creates list SDTM with elements DM, AE, MH
+SDTM <- list(
+  DM = rtables::ex_adsl %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM),
+  AE = rtables::ex_adae %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, AESEQ, AETERM, AEDECOD),
+  MH = rtables::ex_admh %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, MHSEQ, MHTERM, MHDECOD, MHBODSYS)
+)
 
 
+
+
+
+ADaM <- list(
+  ADSL = rtables::ex_adsl,
+  ADAE = rtables::ex_adae,
+  ADMH = rtables::ex_admh
+)
--- README.md
+++ README.md
@@ -10,5 +10,3 @@ remotes::install_github("Roche-GSK/admiral")
 remotes::install_github("Roche/respectables")
 remotes::install_github("Roche/synthetic.cdisc.data")
 ```
-
-
