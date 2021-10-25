f9cfe6e4f20858d18a9a94e20a9611438e792d5c
Kieran Martin <kieran.martin@roche.com>
2021-10-25 15:43:19 +0100

    Update vs_date_gen.R

--- programs/sdtm/functions/vs_date_gen.R
+++ programs/sdtm/functions/vs_date_gen.R
@@ -1,6 +1,3 @@
 date_gen <- function(.df, n){
-  start_date <- .df$RFSTDTC
-  vis_num <- .df$VISITNUM
-  rand_posixct(start = start_date, end = start_date + lubridate::years(2))
-
+  as.Date(.df$RFSTDTC) + lubridate::days((.df$VISITNUM-1) * 14)
 }
