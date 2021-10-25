66298d1c2c33bebc1a5e743fa764e77efaf77dcc
xw2375 <xw2375@gmail.com>
2021-10-25 01:11:04 +0000

    added AE and VS

--- programs/tables_static.R
+++ programs/tables_static.R
@@ -2,49 +2,64 @@
 library(rtables)
 library(dplyr)
 
-source("tables_func.R")
+source("programs/tables_func.R")
 
 # read ADaM data
+adsl <- readRDS('data_demo/ADSL.rds')
+adae <- readRDS('data_demo/ADAE.rds')
+advs <- readRDS('data_demo/ADVS.rds')
 
+# Filter ADaM data
 adsl__ITT <- adsl %>%
   filter(ITTFL == "Y")
+adsl__SE <- adsl %>%
+  filter(SAFFL == "Y")
+adae__SE <- adae %>%
+  filter(SAFFL == 'Y')
+advs__SE <- advs %>%
+  filter(SAFFL == 'Y')
 
-adsl__AGEG18 <- asl %>%
-  filter(AGE > 18)
 
 
 # Demographics Tables ----
 
 tbl_dm__ITT <- t_dm(
-  adsl = adsl__ITT,
+  adsl = adsl__ITT[c('SEX')],
   title = 'Demographic and Baseline Characteristics',
   subtitle = 'Protocol: AB12345',
   main_footer ='Test footnotes'
 )
 
 # tbl_dm__ITT
-
 # Viewer(tbl_dm__ITT)
 
 
 # lpp numeric
 # - Maximum lines per page including (re)printed header and context rows
-# min_siblings  numeric
-# - Minimum sibling rows which must appear on either side of pagination row for a mid-subtable split to be valid. Defaults to 2.
-# nosplitin character
-# - List of names of sub-tables where page-breaks are not allowed, regardless of other considerations. Defaults to none.
-
 paginate_table(tbl_dm__ITT)  ## figure out pagination
 paginate_table(tbl_dm__ITT, lpp = 15, min_siblings = 2, nosplitin = NULL)
-
 
 
 export_as_txt(tbl_dm__ITT, paginate = T, file = 'outputs/t_dm_IT.out')
 export_as_pdf(tbl_dm__ITT, paginate = T, file = 'outputs/t_dm_IT.pdf')
 
 
+# AE Tables ----
+# t_ae(
+#   adae = adae__SE,
+#   adsl = adsl__SE,
+#   title = '',
+#   subtitle = '',
+#   main_footer =''
+# )
 
-
-
+# VS Tables ----
+# t_vs(
+#   advs = advs__SE,
+#   adsl = adsl__SE,
+#   title = '',
+#   subtitle = '',
+#   main_footer =''
+# )
 
 
