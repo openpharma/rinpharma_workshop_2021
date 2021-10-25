a6817a7c4968c72c185fb82172053b571f47529b
neitmant <thomas.neitmann@roche.com>
2021-10-25 10:57:55 +0200

    finalize demo data

--- programs/ADaM.R
+++ programs/ADaM.R
@@ -1,23 +1,47 @@
-
-library(admiral)           # https://roche-gsk.github.io/admiral/
 library(dplyr)
 
-# source("SDTM.R") # creates list SDTM with elements DM, AE, MH
-SDTM <- list(
-  DM = rtables::ex_adsl %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM),
-  AE = rtables::ex_adae %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, AESEQ, AETERM, AEDECOD),
-  MH = rtables::ex_admh %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, MHSEQ, MHTERM, MHDECOD, MHBODSYS)
-)
+dtm_to_dtc <- function(dtm) {
+  gsub(" ", "T", as.character(dtm))
+}
 
-saveRDS(rtables::ex_adsl %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM), "data_demo/DM.rds")
-saveRDS(rtables::ex_adae %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, AESEQ, AETERM, AEDECOD), "data_demo/AE.rds")
-saveRDS(rtables::ex_admh %>% select(USUBJID, STUDYID, SITEID, ARM, ACTARM, MHSEQ, MHTERM, MHDECOD, MHBODSYS), "data_demo/MH.rds")
+DM <- rtables::ex_adsl %>%
+  transmute(STUDYID, USUBJID, SITEID, ARM, ACTARM, BRTHDTC = as.character(RANDDT - AGE * 365), DTHDTC = as.character(DTHDT))
+AE <- rtables::ex_adae %>%
+  transmute(
+    USUBJID, STUDYID, SITEID, ARM, ACTARM, AESEQ, AESTDTC = dtm_to_dtc(ASTDTM),
+    AEENDTC = dtm_to_dtc(AENDTM), AETERM, AEDECOD, AEBODSYS, AESEV, AESER, AETOXGR
+  )
+VS <- rtables::ex_advs %>%
+  transmute(
+    USUBJID, STUDYID, SITEID, DOMAIN = "VS", ARM, ACTARM, VSSEQ, VSTEST, VSTESTCD,
+    VSDTC = dtm_to_dtc(ADTM), VSSTRESC, VSORRES = VSSTRESC, VSSTRESN = as.numeric(VSSTRESC)
+  )
+DS <- rtables::ex_adsl %>%
+  transmute(
+    STUDYID, USUBJID, RANDDT, ENRLDT = RANDDT - sample(0:5, nrow(.), replace = TRUE),
+    EOSDT
+  ) %>%
+  tidyr::pivot_longer(RANDDT:EOSDT, names_to = "DSDECOD", values_to = "DSSTDTC") %>%
+  mutate(
+    DSSTDTC = as.character(DSSTDTC),
+    DSDECOD = case_when(
+      DSDECOD == "RANDDT" ~ "RANDOMIZED",
+      DSDECOD == "ENRLDT" ~ "ENROLLED",
+      DSDECOD == "EOSDT" ~ sample(c("DEATH", "WITHDRAWAL BY SUBJECT", "PHYSICIAN DECISION"), nrow(.), replace = TRUE)
+    ),
+    DSCAT = if_else(DSDECOD %in% c("RANDOMIZED", "ENROLLED"), "PROTOCOL MILESTONE", "DISPOSITION EVENT")
+  )
 
-saveRDS(rtables::ex_adsl, "data_demo/ADSL.rds")
+saveRDS(DM, "data_demo/DM.rds")
+saveRDS(AE, "data_demo/AE.rds")
+saveRDS(VS, "data_demo/VS.rds")
+saveRDS(DS, "data_demo/DS.rds")
+
+rtables::ex_adsl %>%
+  mutate(
+    TRTSDT = lubridate::date(TRTSDTM),
+    TRTEDT = lubridate::date(TRTEDTM)
+  ) %>%
+  saveRDS("data_demo/ADSL.rds")
 saveRDS(rtables::ex_adae, "data_demo/ADAE.rds")
-saveRDS(rtables::ex_admh, "data_demo/ADMH.rds")
-ADaM <- list(
-  ADSL = rtables::ex_adsl,
-  ADAE = rtables::ex_adae,
-  ADMH = rtables::ex_admh
-)
+saveRDS(rtables::ex_advs, "data_demo/ADVS.rds")
